80dd9bd2400cc433660900808854b1abd0275550
Matin Sarahi <matinsarahi@gmail.com>
2021-06-11 16:59:54 -0400

    journal entry ui ish

--- src/components/JournalEntry.js
+++ src/components/JournalEntry.js
@@ -4,11 +4,23 @@ import styled from "styled-components";
 
 const JournalEntry = ({date, title }) => {
   return (
-    <div className="card-container">
+    <StyledCardContainer>
       <h2>{date}</h2>
       <p>{title}</p>
-    </div>
+    </StyledCardContainer>
   );
 };
 
+const StyledCardContainer = styled(motion.div)`
+  width: 25vw;
+  height: 10vh;
+  box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.1),
+      0px 20px 20px rgba(0, 0, 0, 0.1);
+  border-radius: 20px;
+  overflow: hidden;
+  h2, p {
+    padding-left: 1vw;
+    padding-top: 1.3vh;
+  }
+`
 export default JournalEntry;
